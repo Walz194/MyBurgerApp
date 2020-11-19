ffcb49c1d03a12b80e43788c7110576e39988e3d
Olawale <olawale.akinodanye@gmail.com>
2020-11-19 22:14:27 +0100

    React forms

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import registerServiceWorker from "./registerServiceWorker";
+
+
+const app = (
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+)
+ReactDOM.render(app, document.getElementById("root"));
+registerServiceWorker();
